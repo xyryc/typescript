f79fd8fc339dc7203a7c6db7291b9e0f2c406c55
xyryc <amdanikzz@gmail.com>
2025-05-17 02:27:24 +0600

    solved problem 2

--- types/src/index.ts
+++ types/src/index.ts
@@ -7,3 +7,9 @@ function formatString(input: string, toUpper?: boolean): string {
     return input.toUpperCase();
   }
 }
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4.0);
+}
