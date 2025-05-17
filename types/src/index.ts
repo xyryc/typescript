7d9578ce075225ee59923c4aafd7722d1eb06704
xyryc <amdanikzz@gmail.com>
2025-05-17 08:49:05 +0600

    solved problem 8

--- types/src/index.ts
+++ types/src/index.ts
@@ -87,3 +87,15 @@ function getDayType(day: Day): string {
     return "Weekday";
   }
 }
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000);
+  });
+}
